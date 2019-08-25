f0eb20277b4921fa136a74af3d881846512b916e
Samuel <muka@outlook.com>
2019-08-24 21:06:00 -0300

    Fisrt Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Alias iste quidem voluptatem non eaque, ut quas aliquam nisi aperiam
+         repudiandae odit, modi quaerat error, eum reprehenderit maiores. Recusandae, impedit veritatis?</p>
+@endsection
